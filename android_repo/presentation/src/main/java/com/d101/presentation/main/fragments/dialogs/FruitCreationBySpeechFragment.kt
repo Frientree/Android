f55d316a55fccad7d6e80809f7b0e45300e154f9
이호성 <ghtjd8264@naver.com>
2024-02-12 02:05:03 +0900

    feat: music stop on recording
    녹음 시간 동안 배경음 끄기
    
    Change-Id: Ibd2059cbcea33deddba2a77dc3f999f50561b505

--- android_repo/presentation/src/main/java/com/d101/presentation/main/fragments/dialogs/FruitCreationBySpeechFragment.kt
+++ android_repo/presentation/src/main/java/com/d101/presentation/main/fragments/dialogs/FruitCreationBySpeechFragment.kt
@@ -1,5 +1,6 @@
 package com.d101.presentation.main.fragments.dialogs
 
+import android.content.Intent
 import android.media.MediaRecorder
 import android.os.Build
 import android.os.Bundle
@@ -9,6 +10,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.d101.presentation.R
 import com.d101.presentation.databinding.FragmentFruitCreationBySpeechBinding
 import com.d101.presentation.main.viewmodel.FruitCreateViewModel
@@ -120,12 +122,22 @@ class FruitCreationBySpeechFragment : Fragment() {
         recorder = null
     }
 
+    override fun onStart() {
+        super.onStart()
+        LocalBroadcastManager.getInstance(requireContext()).sendBroadcast(
+            Intent("PAUSE"),
+        )
+    }
+
     override fun onStop() {
         super.onStop()
         recorder?.release()
         recorder = null
 
         timerTask.cancel()
+        LocalBroadcastManager.getInstance(requireContext()).sendBroadcast(
+            Intent("PLAY"),
+        )
     }
 
     override fun onDestroyView() {
