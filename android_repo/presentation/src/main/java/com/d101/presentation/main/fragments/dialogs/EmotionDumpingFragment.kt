d0b8d95e0819a38fc6e662612b9dfee169fe1af6
정수진 <dmg06017@naver.com>
2024-02-13 04:04:35 +0900

    feat: add boom animation
    블랙홀이 사라진 후 폭발하는 애니메이션 추가
    
    Change-Id: Iebdcd48ed74c4f2ecb8d8fffadaaa94a614e4eaa

--- android_repo/presentation/src/main/java/com/d101/presentation/main/fragments/dialogs/EmotionDumpingFragment.kt
+++ android_repo/presentation/src/main/java/com/d101/presentation/main/fragments/dialogs/EmotionDumpingFragment.kt
@@ -1,5 +1,10 @@
 package com.d101.presentation.main.fragments.dialogs
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
@@ -15,6 +20,11 @@ class EmotionDumpingFragment : DialogFragment() {
 
     private var _binding: FragmentEmotionDumpingBinding? = null
     private val binding get() = _binding!!
+
+    private val animatorBigger = ValueAnimator.ofFloat(1f, 1.5f)
+    private val animatorSmaller = ValueAnimator.ofFloat(1.5f, 0f)
+
+    val animatorSet = AnimatorSet()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,10 +43,62 @@ class EmotionDumpingFragment : DialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        animatorBigger.duration = 300
+        animatorSmaller.duration = 700
+        animatorBigger.addUpdateListener { animation ->
+            val value = animation.animatedValue as Float
+            binding.blackHoleLottieView.scaleX = value
+            binding.blackHoleLottieView.scaleY = value
+        }
+        animatorSmaller.addUpdateListener { animation ->
+            val value = animation.animatedValue as Float
+            binding.blackHoleLottieView.scaleX = value
+            binding.blackHoleLottieView.scaleY = value
+        }
+
+        animatorSet.play(animatorSmaller).after(animatorBigger)
+
+        val fadeOutTextView = ObjectAnimator.ofFloat(
+            binding.blackHoleTextView,
+            "alpha",
+            1f,
+            0f,
+        )
+        val fadeOutButton = ObjectAnimator.ofFloat(
+            binding.blackHoleButton,
+            "alpha",
+            1f,
+            0f,
+        )
+        fadeOutButton.duration = 500
+        fadeOutTextView.duration = 500
+
+        binding.blackHoleButton.setOnClickListener {
+            animatorSet.start()
+            fadeOutButton.start()
+            fadeOutTextView.start()
+        }
+        animatorSet.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                binding.boomLottieView.visibility = View.VISIBLE
+                binding.boomLottieView.playAnimation()
+            }
+        })
+
+        binding.boomLottieView.addAnimatorListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                dialog?.dismiss()
+            }
+        })
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        binding.boomLottieView.removeAllAnimatorListeners()
+        animatorSet.removeAllListeners()
+        animatorBigger.removeAllUpdateListeners()
+        animatorSmaller.removeAllUpdateListeners()
         _binding = null
     }
 }
